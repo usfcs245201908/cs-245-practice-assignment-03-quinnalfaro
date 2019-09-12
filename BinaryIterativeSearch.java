174221156a5b3e2900c7a8e9e986a862c0209cd8
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-09-12 14:10:53 -0700

    Add files via upload

--- BinaryIterativeSearch.java
+++ BinaryIterativeSearch.java
@@ -1,10 +1,24 @@
 public class BinaryIterativeSearch implements Practice03Search {
 
 	public int search(int [] arr, int target){
+		int start = 0;
+		int end = arr.length - 1;
+
+		while (start <= end) {
+			int middle = (start + end) / 2;
+			if (target < arr[middle]) {
+				end = middle - 1;
+			} else if (target > arr[middle]) {
+				start = middle + 1;
+			} else if (target == arr[middle]) {
+				return middle;
+			}
+		}
+
 		return -1;
 	}
 
 	public String searchName(){
-		return  "Binary Iterative Search";
+		return "binary iterative search";
 	}
 }
