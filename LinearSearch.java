a9c2d5c7398a14ce1a39141b477525de00d2c081
Quinn Daniel Alfaro <35777556+quinnalfaro@users.noreply.github.com>
2019-09-12 13:22:35 -0700

    Add files via upload

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,11 @@
 public class LinearSearch implements Practice03Search {
 
 	public int search(int [] arr, int target){
+		for (int i = 0; i < arr.length; i ++){
+			if(arr[i] == (target)){
+				return i;
+			}
+		}
 		return -1;
 	}
 
